c412831c066de260c8783f09ec0e78b08a19fcd2
Mickael <65185697+mickaelluis@users.noreply.github.com>
2025-02-12 16:34:32 -0300

    Responsividade

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,10 @@
 import React from "react"
 import {
     Box,
-    Heading,
     Container,
     Text,
     Button,
     Stack,
-    Icon,
-    useColorModeValue,
-    createIcon,
-    Link,
     Flex,
     Image,
   } from '@chakra-ui/react'
@@ -19,53 +14,62 @@ import {
   import TiktokIcon from '../assets/images/tik-tok.svg';
   import twitchIcon from '../assets/images/twitch.svg';
   import discordIcon from '../assets/images/discord.svg';
+  import avatar from '../assets/images/Avatar.jpeg';
 
-
-
+//base → Aplica para todas as telas menores que md (mobile-first).
+//md → Aplica a partir de 48em (768px).
+//lg → Aplica a partir de 64em (1024px).
+ 
 
 export default function Home() {
     return(
             <>
-        <Container maxW={'3xl'}  >
+        <Container maxW={'3xl'}   >
             <Stack as={Box}
           textAlign={'center'}
           spacing={{ base: 8, md: 14 }}
-          py={{ base: 20, md: 36 }} alignItems={'center'} >
-                <Box bg='tomato' w='30rem' h={'50rem'} p={4} color='white' borderRadius={'100px'}   >
-                    <Text fontSize={'6xl'} padding={'2rem 0 0 0'}>
+          py={{ base: '12rem', md: 36 }} alignItems={'center'} >
+                <Box bg='tomato'  p={4} color='white' borderRadius={'50px'} w={{ base: "75%", md: "26em", lg: "22em" }}  h={{ base: "10%", md: "35em", lg: "35em" }} mt="-7rem" >
+                    <Text fontSize={{ base: "2.5em", md: "2.5em", lg: "2em" }} padding={'2rem 0 0 0'}>
                         @Capitão
                     </Text>
-                    
-                    <Flex flexDirection={'column'} padding={'10em 5rem 0 5rem'}  gap='2'>
+                    <Box display="flex" justifyContent="center" mt='2rem'>
+                    <Image
+                        src={avatar}  
+                        borderRadius='full'
+                        boxSize='150px' 
+                    />
+                    </Box>
+                    <Flex flexDirection={'column'}  padding={'0 5rem 0 5rem'}  mt={{ base:' 3rem', md: '3rem', lg: '3rem' }}  gap='2'> 
                              
                         <Button as="a"  href="https://www.youtube.com/@capitao2729" leftIcon={<Image src={YoutubeIcon} boxSize="30px"  sx={{ filter: "brightness(0) invert(1)"}} />} colorScheme="red"  >
-                            <Text fontSize={'4xl'}>
+                            <Text fontSize={{ base: "1.2em", md: "1.5em", lg: "1em" }}>
                                 Youtube 
                             </Text>
                         </Button>   
 
                         
                         <Button as="a" href="https://www.tiktok.com/@capita_o"  leftIcon={<Image src={TiktokIcon} boxSize="30px"  sx={{ filter: "brightness(0) invert(1)"}} />}  bg="black" _hover={{ bg: "gray.800" }} >
-                            <Text fontSize={'4xl'}>
+                            <Text fontSize={{ base: "1em", md: "1.5em", lg: "1em" }}>
                                 Tiktok
                             </Text>
                         </Button>
                         
 
                         <Button as="a" href="https://www.twitch.tv/lcapitaoj" leftIcon={<Image src={twitchIcon} boxSize="30px" sx={{ filter: "brightness(0) invert(1)"}} />} colorScheme="purple"  >
-                            <Text fontSize={'4xl'}>
+                            <Text fontSize={{ base: "1.2em", md: "1.5em", lg: "1em" }}>
                                 Twitch
                             </Text>
                         </Button>
 
                         <Button as="a" href="https://discord.gg/TRb8KEBRUx" leftIcon={<Image src={discordIcon} boxSize="30px" sx={{ filter: "brightness(0) invert(1)"}} />} bg='#2B2BE0'  _hover={{bg:'#1B51E0'}}  >
-                            <Text fontSize={'4xl'}>
+                            <Text fontSize={{ base: "1.2em", md: "1.5em", lg: "1em" }}>
                                 Discord
                             </Text>
                         </Button>
                         
-                        <Text>
-                            Se inscreva em todos meus canais, 
+                        <Text fontSize={{ base: "1.2em", md: "1.5em", lg: "1.3em" }}>
+                            Se inscreva em todos meus canais.
                         </Text>
                     </Flex>
                 </Box>
--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -2,10 +2,21 @@ import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
   breakpoints: {
-    sm: '30em',  
-    md: '48em',  
-    lg: '62em',  
-    xl: '80em',  
+    xs: "20em",  // 320px (Celulares pequenos)
+    sm: "30em",  // 480px (Celulares médios)
+    md: "48em",  // 768px (Tablets)
+    lg: "64em",  // 1024px (Notebooks)
+    xl: "80em",  // 1280px (Desktops)
+    "2xl": "96em", // 1536px (Telas grandes)
+  },
+  styles: {
+    global: {
+      "html, body": {
+        backgroundColor: "gray.100",
+        color: "gray.800",
+        fontSize: { base: "1em", md: "1.125em", lg: "1.25em" }, // Escala o tamanho do texto
+      },
+    },
   },
 });
 
