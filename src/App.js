8fec25ff86b0b4939979bd0f472e15d9e2cea235
Mickael <65185697+mickaelluis@users.noreply.github.com>
2025-02-14 16:03:34 -0300

    designer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import theme from './styles/theme/theme.ts';
 import { BrowserRouter } from "react-router-dom";
 const App = () => {
-    return (
+    return (  
     <BrowserRouter basename="/CapitaoRedes">
         <ChakraProvider theme={theme}>
             <AppRoutes />
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -14,36 +14,50 @@ import {
   import TiktokIcon from '../assets/images/tik-tok.svg';
   import twitchIcon from '../assets/images/twitch.svg';
   import discordIcon from '../assets/images/discord.svg';
-  import avatar from '../assets/images/Avatar.jpeg';
-
+  import avatar from '../assets/images/Avatar.svg';
+  import dominio from '../assets/images/dominio.svg';
 //base → Aplica para todas as telas menores que md (mobile-first).
 //md → Aplica a partir de 48em (768px).
 //lg → Aplica a partir de 64em (1024px).
- 
+
+
 
 export default function Home() {
     return(
             <>
-        <Container maxW={'3xl'}   >
+        <Container
+            bgImage={dominio}
+            bgSize="cover"  bgPosition="center" bgRepeat="no-repeat"   
+            minW="100vw"  
+            minH="100vh"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            >
             <Stack as={Box}
           textAlign={'center'}
           spacing={{ base: 8, md: 14 }}
-          py={{ base: '12rem', md: 36 }} alignItems={'center'} >
-                <Box bg='tomato'  p={4} color='white' borderRadius={'50px'} w={{ base: "100%", md: "26em", lg: "22em" }}  h={{ base: "10%", md: "35em", lg: "35em" }} mt="-7rem" >
-                    <Text fontSize={{ base: "2.5em", md: "2.5em", lg: "2em" }} padding={'2rem 0 0 0'}>
-                        @Capitão
-                    </Text>
+          py={{ base: '12rem', md: 36 }} alignItems={'center'}   >
+                <Box    bg="radial-gradient(circle, #d60000 10%, #8b0000 80%)"
+                        boxShadow="inset 0px 0px 100px 20px rgba(0, 0, 0, 0.5)" 
+                        p={4} color='white' borderRadius={'50px'} 
+                        w={{ base: "100%", md: "26em", lg: "22em" }}  
+                        h={{ base: "10%", md: "35em", lg: "35em" }} 
+                        mt="-7rem" 
+                                         >
                     <Box display="flex" justifyContent="center" mt='2rem'>
                     <Image
                         src={avatar}  
-                        borderRadius='full'
-                        boxSize='150px' 
+                        boxSize={{ base: "10em", md: "8em", lg: "10em" }}
                     />
                     </Box>
+                    <Text fontSize={{ base: "2.5em", md: "2.5em", lg: "2em" }} padding={'2rem 0 0 0'} >
+                        @Capitão
+                    </Text>
                     <Flex flexDirection={'column'}  padding={'0 5rem 0 5rem'}  mt={{ base:' 3rem', md: '3rem', lg: '3rem' }}  gap='2'> 
                              
                         <Button as="a"  href="https://www.youtube.com/@capitao2729" leftIcon={<Image src={YoutubeIcon} boxSize="30px"  sx={{ filter: "brightness(0) invert(1)"}} />} colorScheme="red"  >
-                            <Text fontSize={{ base: "0.8em", md: "1.5em", lg: "1em" }}>
+                            <Text fontSize={{ base: "0.8em", md: "1.5em", lg: "1em" }}  >
                                 Youtube 
                             </Text>
                         </Button>   
@@ -68,7 +82,7 @@ export default function Home() {
                             </Text>
                         </Button>
                         
-                        <Text fontSize={{ base: "0.8em", md: "1.5em", lg: "1.3em" }}>
+                        <Text fontSize={{ base: "0.8em", md: "1.5em", lg: "1.3em" }} >
                             Se inscreva em todos meus canais.
                         </Text>
                     </Flex>
